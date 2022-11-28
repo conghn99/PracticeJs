8879f724449911d28b7e24c134a42fa7bc980a51
kong <congvip1999hn@gmail.com>
2022-11-28 20:39:27 +0700

    commit day 4 class

--- BasicJsDay4/Class/Exercise1.js
+++ BasicJsDay4/Class/Exercise1.js
@@ -41,3 +41,36 @@ var animals = [
         yearOld : 91
     }
 ]
+
+// Bai 6
+function checkOldestAge(arr) {
+    empArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        empArr.push(arr[i].yearOld);
+    }
+    let maxAge = Math.max(...empArr);
+    console.log(arr.filter(x => x.yearOld === maxAge));
+}
+
+checkOldestAge(animals);
+
+// function Car(brand, price, country) {
+//     this.brand = brand;
+//     this.price = price;
+//     this.country = country
+//     this.speed = function() {
+//         console.log(`${this.brand} nhanh vc`)
+//     }
+// }
+
+// var car = new Car("Limousine", 1000000000, "Germany");
+// car.speed();
+
+const sampleProto = {
+    name : "abc",
+    greet() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+sample_1 = Object.create(sampleProto)
