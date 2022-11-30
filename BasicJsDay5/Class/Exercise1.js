4b5fef30c5462a02471f08a0e9bb2977fc256d40
kong <congvip1999hn@gmail.com>
2022-11-30 20:06:17 +0700

    commit day 5_class_1

---
+++ BasicJsDay5/Class/Exercise1.js
@@ -0,0 +1,8 @@
+// Bai 1
+console.log(document.querySelector(".text-content").firstElementChild);
+// Bai 2
+console.log(document.querySelector(".text-content").children[1]);
+// Bai 3
+console.log(Array.from(document.querySelector(".text-content").children));
+// Bai 4
+console.log(Array.from(document.querySelectorAll("li")));
