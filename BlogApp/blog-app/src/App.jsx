f35243c24cfb7b9dbf40180d8d974bdda5fcda00
kong <congvip1999hn@gmail.com>
2023-02-15 21:29:04 +0700

    comit blog-4

--- BlogApp/blog-app/src/App.jsx
+++ BlogApp/blog-app/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './component/Layout/Layout'
+import BlogDetail from './pages/blog/BlogDetail'
+import Home from './pages/home/Home'
+import Search from './pages/search/Search'
+import TagDetail from './pages/tags/TagDetail'
+import TagList from './pages/tags/TagList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='blogs/:blogId' element={<BlogDetail />}></Route>
+          <Route path='search' element={<Search />}></Route>
+          <Route path='tags'>
+            <Route index element={<TagList />} />
+            <Route path=':tagId/:tagName' element={<TagDetail />}></Route>
+          </Route>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
