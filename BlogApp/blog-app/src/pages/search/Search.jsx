ef88cd84c02818669ad82eaccf9c9ef65005883b
kong <congvip1999hn@gmail.com>
2023-02-17 09:48:37 +0700

    commit fix search

--- BlogApp/blog-app/src/pages/search/Search.jsx
+++ BlogApp/blog-app/src/pages/search/Search.jsx
@@ -1,22 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useGetBlogsByKeywordQuery } from '../../app/Service/blog.service';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from "yup";
 
 function Search() {
   const [title, setTitle] = useState("");
   const {data, isLoading} = useGetBlogsByKeywordQuery(title);
-
-  const schema = yup.object({
-    title: yup.string().required(),
-    age: yup.number().positive().integer().required(),
-  }).required();
-
-  const { register, handleSubmit, formState:{ errors } } = useForm({
-    resolver: yupResolver(schema)
-  });
-  const onSubmit = data => console.log(data);
 
   if (isLoading) {
     return <h2>Loading....</h2>
@@ -32,10 +22,10 @@ function Search() {
             <div className="post-meta"></div>
         </header>
         <div id="searchbox">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form>
             <input id="searchInput" autoFocus placeholder="Tìm kiếm bài viết" aria-label="search" type="search"
-                autoComplete="off" value={title} onChange={(e) => setTitle(e.target.value)} {...register("title")}/>
-            <span>{errors.title?.message}</span>
+                autoComplete="off" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <span></span>
           </form>
           <ul id="searchResults" aria-label="search results">
             {title !== "" && data.length > 0 && data.map((b, i) => (
